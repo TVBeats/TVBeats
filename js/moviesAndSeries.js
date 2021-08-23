b702c4d69f1390a8230437527b5f4f831c78ce62
EmadIdris <emadidris@outlook.com>
2021-08-23 12:59:43 +0300

    finish Js Cart

--- js/moviesAndSeries.js
+++ js/moviesAndSeries.js
@@ -1,17 +1,102 @@
-'use srtict'
+'use strict';
+window.onload = function(){
+	//cart box
+	const iconShopping = document.querySelector('.iconShopping');
+	const cartCloseBtn = document.querySelector('.fa-close');
+	const cartBox = document.querySelector('.cartBox');
+	iconShopping.addEventListener("click",function(){
+		cartBox.classList.add('active');
+	});
+	cartCloseBtn.addEventListener("click",function(){
+		cartBox.classList.remove('active');
+	});
 
-// slide show
-let myIndex = 0;
-carousel();
 
-function carousel() {
-  let i;
-  let x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2500);
+	// adding data to localstorage
+	const attToCartBtn = document.getElementsByClassName('attToCart');
+	let items = [];
+	for(let i=0; i<attToCartBtn.length; i++){
+		attToCartBtn[i].addEventListener("click",function(e){
+			if(typeof(Storage) !== 'undefined'){
+				let item = {
+						id:i+1,
+						name:e.target.parentElement.children[0].textContent,
+						price:e.target.parentElement.children[1].children[0].textContent,
+						no:1
+					};
+				if(JSON.parse(localStorage.getItem('items')) === null){
+					items.push(item);
+					localStorage.setItem("items",JSON.stringify(items));
+					window.location.reload();
+				}else{
+					const localItems = JSON.parse(localStorage.getItem("items"));
+					localItems.map(data=>{
+						if(item.id == data.id){
+							item.no = data.no + 1;
+						}else{
+							items.push(data);
+						}
+					});
+					items.push(item);
+					localStorage.setItem('items',JSON.stringify(items));
+					window.location.reload();
+				}
+			}else{
+				alert('local storage is not working on your browser');
+			}
+		});
+	}
+	// adding data to shopping cart 
+	const iconShoppingP = document.querySelector('.iconShopping p');
+	let no = 0;
+	JSON.parse(localStorage.getItem('items')).map(data=>{
+		no = no+data.no
+;	});
+	iconShoppingP.innerHTML = no;
+
+
+	//adding cartbox data in table
+	const cardBoxTable = cartBox.querySelector('table');
+	let tableData = '';
+	tableData += '<tr><th>S no.</th><th>Item Name</th><th>Item No</th><th>item Price</th><th></th></tr>';
+	if(JSON.parse(localStorage.getItem('items'))[0] === null){
+		tableData += '<tr><td colspan="5">No items found</td></tr>'
+	}else{
+		JSON.parse(localStorage.getItem('items')).map(data=>{
+			tableData += '<tr><th>'+data.id+'</th><th>'+data.name+'</th><th>'+data.no+'</th><th>'+data.price+'</th><th><a href="#" onclick=Delete(this);>Delete</a></th></tr>';
+		});
+	}
+	cardBoxTable.innerHTML = tableData;
 }
+
+// let userNamee = [];
+// function ADD (userName , number ,  email)
+// {
+//   this.userName = userName
+//   this.number = number
+//   this.email = email
+
+//   userNamee.push(this)
+// }
+
+
+// let formEl = document.getElementById('form')
+// formEl.addEventListener('submit' ,showResult)
+
+// function showResult(event)
+// {
+//   event.preventDefault();
+//   let userName = event.target.userName.value;
+//   let number = event.target.number.value;
+//   let email = event.target.email.value;
+//   let newUser = new ADD (userName,number,email )
+//   formEl.reset();
+//   let ulEl= document.createElement('ul')
+// let liEl =document.createElement('li')
+// ulEl.appendChild(liEl)
+// liEl.textContent = `Hello Mr.${this.userName} , Your Number is ${this.number} , Your Email ${this.email}`;
+// }
+
+
+
+
